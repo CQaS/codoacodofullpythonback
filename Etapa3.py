96855ba5018e63605999e631f3c85adffaf347f1
Avila Esteban <cqasss@gmail.com>
2023-07-03 16:37:09 -0300

    class + class

--- Etapa3.py
+++ Etapa3.py
@@ -1,184 +1,23 @@
-import sqlite3
-from flask import Flask,  jsonify, request
+#imporamos la creacion de la tabla
+from conexion import create_table
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+#importamos las clases desde otros Archivos
+from ProductoClass import Producto
+from InventarioClass import Inventario
+from CarritoClass import Carrito
 
-# Configurar la conexión a la base de datos SQLite
-DATABASE = 'inventario.db'
+#se crea la la Base y la Tabla productos   
+create_table()
 
-def get_db_connection():
-    print("Obteniendo conexión...") # Para probar que se ejecuta la función
-    conn = sqlite3.connect(DATABASE, check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-# Crear la tabla 'productos' si no existe
-def create_table():
-    print("Creando tabla productos...") # Para probar que se ejecuta la función
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS productos (
-            codigo INTEGER PRIMARY KEY,
-            descripcion TEXT NOT NULL,
-            cantidad INTEGER NOT NULL,
-            precio REAL NOT NULL
-        )
-    ''')
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-# Verificar si la base de datos existe, si no, crearla y crear la tabla
-def create_database():
-    print("Creando la BD...") # Para probar que se ejecuta la función
-    conn = sqlite3.connect(DATABASE, check_same_thread=False)
-    conn.close()
-    create_table()
-
-# Crear la base de datos y la tabla si no existen
-create_database()
-
-# -------------------------------------------------------------------
-# Definimos la clase "Producto"
-# -------------------------------------------------------------------
-class Producto:
-    def __init__(self, codigo, descripcion, cantidad, precio):
-        self.codigo = codigo
-        self.descripcion = descripcion
-        self.cantidad = cantidad
-        self.precio = precio
-
-    def modificar(self, nueva_descripcion, nueva_cantidad, nuevo_precio):
-        self.descripcion = nueva_descripcion
-        self.cantidad = nueva_cantidad
-        self.precio = nuevo_precio
-
-
-# -------------------------------------------------------------------
-# Definimos la clase "Inventario"
-# -------------------------------------------------------------------
-class Inventario:
-    def __init__(self):
-        self.conexion = get_db_connection()
-        self.cursor = self.conexion.cursor()
-
-    def agregar_producto(self, codigo, descripcion, cantidad, precio):
-        producto_existente = self.consultar_producto(codigo)
-        if producto_existente:
-            return jsonify({'message': 'Ya existe un producto con ese código.'}), 400
-
-        #nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
-        self.cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?)", (codigo, descripcion, cantidad, precio))
-        self.conexion.commit()
-        return jsonify({'message': 'Producto agregado correctamente.'}), 200
-
-    def consultar_producto(self, codigo):
-        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (codigo,))
-        row = self.cursor.fetchone()
-        if row:
-            codigo, descripcion, cantidad, precio = row
-            return Producto(codigo, descripcion, cantidad, precio)
-        return None
-
-    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
-        producto = self.consultar_producto(codigo)
-        if producto:
-            producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
-            self.cursor.execute("UPDATE productos SET descripcion = ?, cantidad = ?, precio = ? WHERE codigo = ?",
-                                (nueva_descripcion, nueva_cantidad, nuevo_precio, codigo))
-            self.conexion.commit()
-            return jsonify({'message': 'Producto modificado correctamente.'}), 200
-        return jsonify({'message': 'Producto no encontrado.'}), 404
-
-    def listar_productos(self):
-        try:
-            self.cursor.execute("SELECT * FROM productos")
-            rows = self.cursor.fetchall()
-            productos = []
-            for row in rows:
-                codigo, descripcion, cantidad, precio = row
-                producto = {'codigo': codigo, 'descripcion': descripcion, 'cantidad': cantidad, 'precio': precio}
-                productos.append(producto)
-            return jsonify(productos), 200
-            
-        except sqlite3.Error as e:            
-            print("Error al consultar la base de datos:", e)
-            return jsonify({'error': 'Error al consultar la base de datos'}), 500
-
-    def eliminar_producto(self, codigo):
-        self.cursor.execute("DELETE FROM productos WHERE codigo = ?", (codigo,))
-        if self.cursor.rowcount > 0:
-            self.conexion.commit()
-            return jsonify({'message': 'Producto eliminado correctamente.'}), 200
-        return jsonify({'message': 'Producto no encontrado.'}), 404
-
-
-# -------------------------------------------------------------------
-# Definimos la clase "Carrito"
-# -------------------------------------------------------------------
-class Carrito:
-    def __init__(self):
-        self.conexion = get_db_connection()
-        self.cursor = self.conexion.cursor()
-        self.items = []
-
-    def agregar(self, codigo, cantidad, inventario):
-        producto = inventario.consultar_producto(codigo)
-        if producto is None:
-            return jsonify({'message': 'El producto no existe.'}), 404
-        if producto.cantidad < cantidad:
-            return jsonify({'message': 'Cantidad en stock insuficiente.'}), 400
-
-        for item in self.items:
-            if item.codigo == codigo:
-                item.cantidad += cantidad
-                self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
-                                    (cantidad, codigo))
-                self.conexion.commit()
-                return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
-
-        nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
-        self.items.append(nuevo_item)
-        self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
-                            (cantidad, codigo))
-        self.conexion.commit()
-        return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
-
-    def quitar(self, codigo, cantidad, inventario):
-        for item in self.items:
-            if item.codigo == codigo:
-                if cantidad > item.cantidad:
-                    return jsonify({'message': 'Cantidad a quitar mayor a la cantidad en el carrito.'}), 400
-                item.cantidad -= cantidad
-                if item.cantidad == 0:
-                    self.items.remove(item)
-                self.cursor.execute("UPDATE productos SET cantidad = cantidad + ? WHERE codigo = ?",
-                                    (cantidad, codigo))
-                self.conexion.commit()
-                return jsonify({'message': 'Producto quitado del carrito correctamente.'}), 200
-
-        return jsonify({'message': 'El producto no se encuentra en el carrito.'}), 404
-
-    def mostrar(self):
-        productos_carrito = []
-        for item in self.items:
-            producto = {'codigo': item.codigo, 'descripcion': item.descripcion, 'cantidad': item.cantidad,
-                        'precio': item.precio}
-            productos_carrito.append(producto)
-        return jsonify(productos_carrito), 200
-
-
-# -------------------------------------------------------------------
-# Configuración y rutas de la API Flask
-# -------------------------------------------------------------------
-#1)	Importación de los módulos y creación de la aplicación Flask
-
+#Programa Principal
 app = Flask(__name__)
 CORS(app)
 
-carrito = Carrito()         # Instanciamos un carrito
-inventario = Inventario()   # Instanciamos un inventario
+carrito = Carrito()
+inventario = Inventario()
 
 # 2 - Ruta para obtener los datos de un producto según su código
 # GET: envía la información haciéndola visible en la URL de la página web.
@@ -207,6 +46,7 @@ def agregar_producto():
     descripcion = request.json.get('descripcion')
     cantidad = request.json.get('cantidad')
     precio = request.json.get('precio')
+    print(codigo, descripcion, cantidad, precio)
     return inventario.agregar_producto(codigo, descripcion, cantidad, precio)
 
 # 5 - Ruta para modificar un producto del inventario
